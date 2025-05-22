a41a0f0a6749740ae46c38e4f47cbc1033c703bc
Seve Savoie Teruel <seve.savoieteruel@planet.com>
2025-05-22 17:27:53 -0400

    (WIP)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  site: 'https://techsev.github.io',
 
   vite: {
     plugins: [tailwindcss()]
   }
-});
+})
